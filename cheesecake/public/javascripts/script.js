4dace127def590461d2a121201bc93e6e76147af
aetherMocker <mocker26@up.edu>
2024-03-01 10:56:55 -0800

    hw4-1

--- cheesecake/public/javascripts/script.js
+++ cheesecake/public/javascripts/script.js
@@ -1,21 +1,54 @@
+/**
+ * script.js
+ * 
+ * all scripts for this proj
+ * includes submitting orders and viewing order info onclick
+ * 
+ * Aether Mocker
+ */
+
 $(document).ready(function () {
+    //scroll on panel that views all 12 months of order_info
     $("#month_button").hover(function () {
          $("#panel").finish().slideToggle("slow");
     });
 
-$("#panel").click(function () {
-    $("#month_button").text($(this).find('a:hover').text());
+    //old ver of viewing order_info (keep)
+    /* $("#panel").click(function () {
+        $("#month_button").text($(this).find('a:hover').text());
+        let month_name = $("#month_button").text();
+        console.log("Month name: " + month_name);
+    });*/
+
+    //when completing an order (clicking submit - order_button)
+    $("#order_button").click(function () {
+        if($("#notes").val() == "vegan") {
+            alert("This bakery doesn't offer vegan cheesecake. We're sorry!");
+        }
+        else {
+            $("#order_form").hide().finish();
+            $("#order_text").text("Thank you! Your order has been placed.");
+            $("#order_text").append(" Order: " + $("#qty").val() + " " + $("input:checked").val() + ", Special Instructions:  " + $("#notes").val());
+        }
+    });
+});
+
+//when viewing order info 
+function monthClick(month_name) {
+    $("#month_button").text(month_name);
+
+    console.log("Month name: " + month_name);
     
     //issue post to the server requesting the orders for a particular month
-    $.post('/orders', function(response) {
-
+    $.post('/orders', { MONTH: month_name }, function(response) {
+        console.log("inside post: " + month_name);
         let plain_counter = 0;
         let cherry_counter = 0;
         let choco_counter = 0;
         
-        
         for(var i = 0; i < response.data.length; i++) {
             let month_topping = response.data[i].topping;
+            console.log(i + " " + month_topping);
 
             if(month_topping === 'plain') {
                 plain_counter += response.data[i].quantity;
@@ -32,18 +65,6 @@ $("#panel").click(function () {
         $("#cherry_info").text(cherry_counter + " cherry");
         $("#choco_info").text(choco_counter + " chocolate");
         $("#plain_info").text(plain_counter + " plain");
-
     });
-});
-
-$("#order_button").click(function () {
-    if($("#notes").val() == "vegan") {
-        alert("This bakery doesn't offer vegan cheesecake. We're sorry!");
-    }
-    else {
-        $("#order_form").hide().finish();
-        $("#order_text").text("Thank you! Your order has been placed.");
-        $("#order_text").append(" Order: " + $("#qty").val() + " " + $("input:checked").val() + ", Special Instructions:  " + $("#notes").val());
-    }
-});
-});
+    console.log("made it here");
+}
--- cheesecake/routes/orders.js
+++ cheesecake/routes/orders.js
@@ -1,17 +1,54 @@
-//https://expressjs.com/en/guide/routing.html
+/**
+ * orders.js
+ * 
+ * gets order data by querying db (gcloud) and posting it
+ * 
+ * Aether Mocker
+ */
 
 var express = require('express');
 var router = express.Router();
+var dbms = require('./dbms');
 
-//sample data
+//data to send
 var cheeseCakeData = [
-  { topping: 'plain', quantity: 3 },
-  { topping: 'cherry', quantity: 2 },
-  { topping: 'chocolate', quantity: 10 },
+  { topping: 'plain', quantity: 0 },
+  { topping: 'cherry', quantity: 0 },
+  { topping: 'chocolate', quantity: 0 },
 ];
 
 /* GET home page. */
+//res = response , get's sent to from script.js. req = request
 router.post('/', function(req, res, next) {
+
+  console.log("made it here: " + res.req.body.MONTH);
+  
+  dbms.query("select * from ORDERS where MONTH= '"+ res.req.body.MONTH +"'", function(err, results) {
+    cheeseCakeData[0].quantity = 0;
+    cheeseCakeData[1].quantity = 0;
+    cheeseCakeData[2].quantity = 0;
+
+    //if there's no error with the sql query
+    if(!(err == null)) {
+
+      for(var i = 0; i < results.length; i++) {
+          console.log(results[i].TOPPING);
+
+          if(results[i].TOPPING == 'plain') {
+            cheeseCakeData[0].quantity += results[i].QUANTITY;
+          }
+          if(results[i].TOPPING == 'cherry') {
+            cheeseCakeData[1].quantity += results[i].QUANTITY;
+          }
+          if(results[i].TOPPING == 'chocolate') {
+            cheeseCakeData[2].quantity += results[i].QUANTITY;
+          }
+      }
+
+      //send data...
+      res.json({ error: null, data: cheeseCakeData});
+    }
+  })
   res.json( {error: null, data: cheeseCakeData });
 });
 
